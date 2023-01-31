7c791b1b8df7ee16a14c6f2ae690fa325e179210
Crowdbotics <team@crowdbotics.com>
2023-01-31 16:17:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sharp-tour-38604",
+  url: "https://sharp-tour-38604.botics.co",
+  api: "https://sharp-tour-38604.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
